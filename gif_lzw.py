83293b5a6f333c597003833fbcbf7b7f78d3da33
Florent Tariolle <florent.dunk@gmail.com>
2025-09-25 14:20:44 +0200

    developped compress function (GIF LZW)

---
+++ gif_lzw.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import sys
+
+def compress(uncompressed):
+    dict_size = 256
+    dictionary = dict((chr(i), i) for i in range(dict_size))
+    w = ""
+    result = []
+    for c in uncompressed:
+        wc = w + c
+        if wc in dictionary:
+            w = wc
+        else:
+            result.append(dictionary[w])
+            dictionary[wc] = dict_size
+            dict_size += 1
+            w = c
+    if w:
+        result.append(dictionary[w])
+    return result
+
+def main() -> int:
+	# Image generation
+    height, width = 100, 100
+    image = np.zeros((height, width), dtype=np.uint8)
+    image[20:80, 20:80] = 200
+    image[40:60, 40:60] = 100
+    flat_image = image.flatten()
+    uncompressed = ''.join(chr(p) for p in flat_image)  # As bytes
+    print(f"Approximate size without compression: {len(uncompressed)} bits")
+
+    compressed_codes = compress(uncompressed)
+    num_codes = len(compressed_codes)
+    max_code = max(compressed_codes)
+    bits_needed = max(9, (max_code.bit_length()))
+    approx_size = (num_codes * bits_needed) // 8 + ((num_codes * bits_needed) % 8 > 0)
+
+    print(f"Number of codes: {num_codes}")
+    print(f"Approximate size with compression: {approx_size} bits")
+
+    return 
+
+if __name__ == "__main__":
+    sys.exit(main())
